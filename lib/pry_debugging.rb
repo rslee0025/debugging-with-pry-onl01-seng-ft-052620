d1304926a8bbb5a01224da4bc8091597f625d06d
Rachel S. Lee <rslee0025@gmail.com>
2020-05-27 02:34:51 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,11 +1,12 @@
+=begin
 def plus_two(num)
 	num + 2
 	num 
 end
+=end
 
-#def plus_two(num)
- # num + 2
-  #sum = 5
-  #return sum
-  
-#end
+def plus_two(num)
+  num + 2
+  sum = 5
+  return sum
+end
